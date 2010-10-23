17638ffd6dae2ccb0dc32c801ccb74342536fb6d
Heliosmaster <helios89@gmail.com>
2010-10-23 18:46:49 +0200

    refactoring stats and moving code to the model

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -39,24 +39,7 @@ class RootController < ApplicationController
   #this will provide stats info about the url
   def info
     @showurl = Url.find_by_mini(params[:mini])
-    hit = Array.new
-    (1..30).each do |n|
-    m = n-1
-    hit[n]=@showurl.views.where(:created_at => (Time.now.midnight - n.day)..(Time.now.midnight - m.day)).size
-    end
-    hit[0] = @showurl.views.where(:created_at => (Time.now.midnight - 1.day)..Time.now).size
-    hit.reverse!
-    hit_s = ""
-    hit.each {|h| hit_s += h.to_s + "," }
-    hit_s.chop!
-    max_hit = hit.sort[hit.length-1].to_s
-   #Arrays with the days of the last month (one every five) to display in chart
-    days = ""
-    (0..6).each do |n|
-    m = (6-n)*5
-    days += "|"+ (Time.now.midnight - m.day).strftime("%d %b")
-    end
-    @chart = "http://chart.apis.google.com/chart?chxl=0:"+days+"&chxp=0,1,5,10,15,20,25,30&chxr=0,1,30&chxs=0,000000,12.5,0,l,676767&chxt=x&chs=400x300&cht=lc&chco=1500FF&chds=0,"+max_hit+"&chd=t:"+hit_s+"&chls=1&chtt=Page+views+in+the+last+30+days"
+    @chart = @showurl.get_stats_chart
     if @showurl.nil?
       flash[:notice] = "URL not found"
       redirect_to root_path
