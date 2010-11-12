05095ea6d868a53a8768b0aeea58825446acccbe
Heliosmaster <helios89@gmail.com>
2010-11-12 18:41:19 +0100

    block clarified, TLD check implemented

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -6,8 +6,14 @@ class RootController < ApplicationController
   end
   
   def create
-    unless params[:url][:address].empty? 
-      @showurl = Url.find_by_address(params[:url][:address])
+    p = params[:url][:address]
+    if p.empty? || !Url.has_valid_TLD?(p)
+      respond_to do |format|
+        format.html {redirect_to root_path}  
+        format.js {render :inline => "alert('Please insert a valid URL')"}
+      end
+    else
+      @showurl = Url.find_by_address(p)
       if @showurl.nil?
         @showurl = Url.create(params[:url])
         @showurl.remote_ip = request.ip
@@ -21,12 +27,7 @@ class RootController < ApplicationController
         format.json {render :json => @showurl.to_json(request.host) }
         format.xml  {render :xml => @showurl.to_xml(request.host) }
         format.js
-      end  
-    else
-      respond_to do |format|
-        format.html {redirect_to root_path}  
-        format.js {render :inline => "alert('url is empty')"}
-      end
+      end      
     end
   end
 
@@ -80,6 +81,5 @@ class RootController < ApplicationController
   end
   
   def new
-
   end
 end
