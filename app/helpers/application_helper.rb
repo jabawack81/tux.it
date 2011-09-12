26308d3bc62450dde3f17de7fede4d14d45a9fe8
Davide Taviani <helios89@gmail.com>
2011-09-12 12:23:20 +0200

    added centering for flash notice

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def flash_div
-    raw flash.keys.collect{|k| "<div class=\"flash #{k}\">#{flash[k]}</div>" }.join unless flash.keys.empty?
+    raw flash.keys.collect{|k| "<div style='text-align:center' class=\"flash #{k}\">#{flash[k]}</div>" }.join unless flash.keys.empty?
   end
   
   def block_header
